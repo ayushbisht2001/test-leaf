b8482f6fd5a107beebd962ebf71ba858250893b6
Ayush Bisht <bisht.ayush2001@gmail.com>
2022-11-19 20:57:39 +0530

    first'

---
+++ code/api/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+
+# @app.route('/')
+# def main():
+ 
+#     return jsonify(person)
+    
+
+
+if __name__ == '__main__':
+    app.run()
